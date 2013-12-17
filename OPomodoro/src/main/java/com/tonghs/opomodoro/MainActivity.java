a7104ebcd83584a4cb2814954e0d5561e675bbee
tonghuashuai <tonghuashuai@126.com>
2013-12-17 17:05:16 +0800

    Initial commit

---
+++ OPomodoro/src/main/java/com/tonghs/opomodoro/MainActivity.java
@@ -0,0 +1,80 @@
+package com.tonghs.opomodoro;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.view.Window;
+import android.widget.ImageButton;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+    public void btn_start_stop(View v){
+        ImageButton btn = (ImageButton)v;
+        //if stopped
+        if (v.getTag().equals("0")){
+            //start
+            btn.setTag("1");
+            btn.setImageDrawable(getResources().getDrawable(R.drawable.stop));
+        } else { //if started
+            //stop
+            btn.setTag("0");
+            btn.setImageDrawable(getResources().getDrawable(R.drawable.play));
+        }
+    }
+}
